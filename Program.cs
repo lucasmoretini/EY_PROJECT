3120f840cc5ff11d2bc541bbf7ef3436bb34809b
lucasmoretini <lukinha.mathias@hotmail.com>
2023-09-23 22:58:15 -0300

    update cors

--- Program.cs
+++ Program.cs
@@ -7,15 +7,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAnyOrigin", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyHeader()
-               .AllowAnyMethod();
-    });
-});
+
 builder.Services.AddScoped<IMongoRepository, MongoRepository>();
 
 var app = builder.Build();
@@ -23,6 +15,10 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+app.UseCors(x => x.AllowAnyHeader()
+.AllowAnyMethod()
+.WithOrigins("http://localhost:3000"));
 
 app.UseAuthorization();
 
