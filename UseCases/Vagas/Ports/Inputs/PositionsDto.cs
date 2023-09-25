b33623b71bcace147497919ffa6074eef4e885b4
Luiz Henrique Cedraz <luiz.cedraz@picpay.com>
2023-09-25 16:17:18 -0300

    Adding empresa to vagas listing

---
+++ UseCases/Vagas/Ports/Inputs/PositionsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EY_Project.UseCases.Empresa.Ports.Input;
+
+namespace EY_Project.UseCases.Vagas.Ports.Inputs
+{
+	public class PositionsDto
+	{
+        public PositionsInput? Vaga { get; set; }
+        public EmpresaInput? Empresa { get; set; }
+
+        public PositionsDto(PositionsInput? vaga, EmpresaInput? empresa)
+        {
+            Vaga = vaga;
+            Empresa = empresa;
+        }
+    }
+}
+
